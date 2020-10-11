88fda79d4f2deb67a6a86bee412a3e6223c81752
Henry Weckermann <henry.weckermann@smail.inf.h-brs.de>
2020-10-11 19:53:54 +0200

    added

---
+++ ocsat.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 11 19:00:16 2020
+
+@author: Henry
+
+OpenWrt configuration summary and analysis tool
+"""
+
+import glob, os, re
+
+os.chdir("Downloads/OpenWrtBackup")
+
+ff = []
+
+for file in glob.glob(r'**\*', recursive=True):
+    if re.match('opkg', file) or re.match('dropbear', file):
+        continue
+    else:
+        ff.append(file)
+
+summary = []
+
+for file in ff:
+    if os.path.isfile(file):
+        try:
+            with open(file, "r") as f:
+                summary.append(f"File Location: {file}\n" + "\n" + f.read() + "\n" + "-"*80 + "\n")
+        except:
+            print(f"Error in file: {file}")
+
+
+with open('your_file.txt', 'w') as f:
+    for item in summary:
+        f.write("%s\n" % item)
